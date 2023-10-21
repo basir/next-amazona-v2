ac79c934d00821308677babd85e9a1622b694419
basir <basir.jafarzadeh@gmail>
2023-10-21 18:01:04 +0330

    05-List-Products-Fix

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <>
       <h2 className="text-2xl py-2">Latest Products</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {/* {data.products.map((product) => (
+        {data.products.map((product) => (
           <ProductItem key={product.slug} product={product} />
-        ))} */}
+        ))}
       </div>
     </>
   )
