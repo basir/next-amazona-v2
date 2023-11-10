dfc4137bb21dd438eeaaac6d4131e9c2fafa1cb0
basir <basir.jafarzadeh@gmail>
2023-11-10 16:29:26 +0330

    06-Create Product Details

--- README.md
+++ README.md
@@ -89,3 +89,10 @@ Welcome to version 2 of next.js amazona. it is coding course to build a full fun
    2. add data.ts
    3. add images
    4. render products
+6. Create Product Details
+   1. create product page
+   2. create 3 columns
+   3. show image in first column
+   4. show product info in second column
+   5. show add to cart action on third column
+   6. add styles
---
+++ app/(front)/product/[slug]/page.tsx
@@ -0,0 +1,74 @@
+import data from '@/lib/data'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ProductDetails({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = data.products.find((x) => x.slug === params.slug)
+  if (!product) {
+    return <div>Product not found</div>
+  }
+  return (
+    <>
+      <div className="my-2">
+        <Link href="/">back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+          ></Image>
+        </div>
+        <div>
+          <ul className="space-y-4">
+            <li>
+              <h1 className="text-xl">{product.name}</h1>
+            </li>
+            <li>
+              {product.rating} of {product.numReviews} reviews
+            </li>
+            <li> {product.brand}</li>
+            <li>
+              <div className="divider"></div>
+            </li>
+            <li>
+              Description: <p>{product.description}</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="card  bg-base-300 shadow-xl mt-3 md:mt-0">
+            <div className="card-body">
+              <div className="mb-2 flex justify-between">
+                <div>Price</div>
+                <div>${product.price}</div>
+              </div>
+              <div className="mb-2 flex justify-between">
+                <div>Status</div>
+                <div>
+                  {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
+                </div>
+              </div>
+              <div className="card-actions justify-center">
+                <button className="btn btn-primary w-full" type="button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
